c2af00a2f7d5b99556914abd09f9add503a33ca7
Noah Batterson <noah@sqr9581.local>
2015-07-26 17:16:00 -0700

    Initial Commit

---
+++ 2Tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2Tip
+//
+//  Created by Noah Batterson on 7/26/15.
+//  Copyright (c) 2015 Noah Batterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
